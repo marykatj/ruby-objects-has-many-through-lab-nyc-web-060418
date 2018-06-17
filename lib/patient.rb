d5ee68e17807e72d51f3e1c07eb0c0b1e829d221
Mary-Kate James <marykatj@gmail.com>
2018-06-17 17:54:46 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,35 +1,22 @@
 require 'pry'
 
 class Patient
-
-  attr_reader :name
-
-  @@all = []
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @@all << self
+
   end
 
-  def self.all
-    @@all
-end
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
 
-def new_appointment(doctor, date)
-  appointment = Appointment.new(date)
-  appointment.doctor = self
-end
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
 
-def appointments
-  Appointment.all.select do |appointment|
-  return appointment.patient == self
-end
-end
-
-def doctors
-  appointments.map do |appointment|
-    appointment.doctor = self
-end.uniq
-end
-
+  def doctors
+    appointments.map{|appointment| appointment.doctor}
+  end
 end
